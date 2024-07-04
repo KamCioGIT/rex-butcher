6a21d05bffc795c32b0ca4e7589651ebe5282a3b
RexshackGaming <rexshackuk@gmail.com>
2024-07-04 14:53:33 +0100

    readme update

--- installation/shared_items.lua
+++
@@ -1,2 +0,0 @@
-    -- butcher
-    ['raw_meat'] = {['name'] = 'raw_meat', ['label'] = 'Raw Meat', ['weight'] = 300, ['type'] = 'item',	['image'] = 'raw_meat.png', ['unique'] = false, ['useable'] = true, ['shouldClose'] = true,	['combinable'] = nil,	['level'] = 0,	['description'] = 'ready for cooking'},
